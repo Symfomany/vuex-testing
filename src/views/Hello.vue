c4aeed9f749bbf83b07a4ee0a2a81c5ec03026a1
Julien Taiwa <taiwa.julien@gmail.com>
2019-08-04 21:44:54 +0200

    blur colot 2

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div style="background-color: blue">
+  <div style="background-color: blue; color: blue">
     <h1 style="color: blue; font-size: 21px">Hello Man</h1>
     <p>Coucu</p>
   </div>
