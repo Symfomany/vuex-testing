d46c608d8efe07796571eac79d4a79a3cef8ad73
Julien Taiwa <taiwa.julien@gmail.com>
2019-08-05 07:48:10 +0200

    add new component

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -3,11 +3,18 @@
   <div>
     <h1 style="color: blue; font-size: 21px">Hello Man</h1>
     <p>Coucu</p>
+    <La></La>
   </div>
 </template>
 
 <script>
-export default {};
+import La from "../components/La";
+
+export default {
+  components: {
+    La
+  }
+};
 </script>
 
 <style>
