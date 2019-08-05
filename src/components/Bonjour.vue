1ecc1a8687fd2a0bb99e08441302cfef64d3a359
Julien Taiwa <taiwa.julien@gmail.com>
2019-08-05 15:55:23 +0200

    modify smiley

--- src/components/Bonjour.vue
+++ src/components/Bonjour.vue
@@ -1,25 +1,29 @@
 <template>
-<div>
-     <header> <slot name="titledefault"><h6>Je suis le contenu par defaut</h6></slot></header>
-    <h3>Bonjour {{  prenom }}</h3>
+  <div>
+    <header>
+      <slot name="titledefault">
+        <h6>Je suis le contenu par defaut :)</h6>
+      </slot>
+    </header>
+    <h3>Bonjour {{ prenom }}</h3>
     <p>Je suis le composant enfant</p>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        prenom: {
-            type: String,
-            default: "Julien"
-        }
-    },
-    mounted(){
-        // console.log(this.$el);
-        // this.$el.style.display="block"
-        // this.$el.firstChild.style.color="red"
-        // this.$el.childNodes[1].style.color="blue"
-        // console.log(this.$el.childNodes[1].getAttribute("classe"))
+  props: {
+    prenom: {
+      type: String,
+      default: "Julien"
     }
-}
+  },
+  mounted() {
+    // console.log(this.$el);
+    // this.$el.style.display="block"
+    // this.$el.firstChild.style.color="red"
+    // this.$el.childNodes[1].style.color="blue"
+    // console.log(this.$el.childNodes[1].getAttribute("classe"))
+  }
+};
 </script>
